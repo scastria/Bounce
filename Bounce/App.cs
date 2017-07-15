66fc4aa5386a4113cc76e50c4c2985a234133dc4
shawn <shawn@spcware.com>
2017-07-15 18:38:04 -0500

    Add Mobile Center packages

--- Bounce/App.cs
+++ Bounce/App.cs
@@ -2,6 +2,10 @@
 using Bounce.Controls;
 using Bounce.Pages;
 using CocosSharp;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Push;
 using Xamarin.Forms;
 
 namespace Bounce
@@ -47,5 +51,14 @@ namespace Bounce
 			_rootPage.Detail = detailNavPage;
 			_rootPage.IsPresented = false;
 		}
-	}
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            MobileCenter.Start("ios=44929f32-ddf6-4a64-a82a-b25aaf1dd0b6;" +
+                   "uwp={Your UWP App secret here};" +
+                   "android=3bd9405a-eead-43c0-8537-e32fe065b505",
+                   typeof(Analytics), typeof(Crashes), typeof(Push));
+        }
+    }
 }
